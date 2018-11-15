7b90589c3a6c4b03b6f546bed57884e8162b4aca
zsyzlyj <344937502@qq.com>
2018-11-15 16:38:58 +0800

    配置了IP，外网可以访问本机的年假系统；下一步把权限分给综合管理员，构建综合管理员的账户列表

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/holiday';
+$config['base_url'] = 'http://10.210.193.40/holiday';
 
 /*
 |--------------------------------------------------------------------------
